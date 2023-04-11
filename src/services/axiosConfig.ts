2785b88eac931948d134944a3d07bc6dddc7efc0
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-04-11 22:17:35 +0200

    feat: change API url

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   // baseURL: 'http://lokalhost:3008/api/v1',
-  baseURL: 'https://rsclone.com/api/v1',
+  baseURL: 'https://books-reading-backend.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
